f43f1efa3e948bd0a9c99a1a0cb05737b694f56c
YourEyes <72436392+YourEyesGG@users.noreply.github.com>
2020-11-23 17:54:33 +0700

    Update and rename __resource.lua to fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,8 +1,15 @@
 fx_version 'adamant'
 game 'gta5'
 
+author 'YourEyes'
 description 'EntityRP Hotwiring'
+version '1.0.0'
 
 client_scripts {
     'client/main.lua'
+}
+
+dependencies {
+    'mythic_progbar',
+    'mythic_notify'
 }
